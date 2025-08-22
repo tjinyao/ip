773600d13a0b8022334c24ce1a515602d13423a8
Your Name <you@example.com>
2025-08-22 11:49:03 +0800

    Level 1

--- src/main/java/Meal.java
+++ src/main/java/Meal.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Meal {
     public static void main(String[] args) {
+        String SEP = "____________________________________________________________";
+
         System.out.println("Hello! I'm Meal\n"
-                + "What can I do for you?\n"
-                + "Bye. Hope to see you again soon!\n");
+                + "What can I do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String line = sc.nextLine().trim();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(SEP);
+            System.out.println(line);
+            System.out.println(SEP);
+        }
+
+        System.out.println(SEP);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEP);
     }
+
 }
