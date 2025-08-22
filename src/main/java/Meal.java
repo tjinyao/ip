2c4189012f38d666acb4dd8aa55e74efa0ce2088
Your Name <you@example.com>
2025-08-22 12:17:35 +0800

    Level-2

--- src/main/java/Meal.java
+++ src/main/java/Meal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Meal {
     public static void main(String[] args) {
@@ -8,14 +9,23 @@ public class Meal {
                 + "What can I do for you?\n");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>();
 
         while (true) {
             String line = sc.nextLine().trim();
             if (line.equals("bye")) {
                 break;
             }
+            if (line.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i + 1) + "." + items.get(i));
+                }
+                System.out.println(SEP);
+                continue;
+            }
+            items.add(line);
             System.out.println(SEP);
-            System.out.println(line);
+            System.out.println("added: " + line);
             System.out.println(SEP);
         }
 
