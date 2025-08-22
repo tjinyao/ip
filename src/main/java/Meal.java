347bd8e6bbacb0da2cf122a11e725cd36ba51395
Your Name <you@example.com>
2025-08-22 11:13:04 +0800

    Level 0

---
+++ src/main/java/Meal.java
@@ -0,0 +1,7 @@
+public class Meal {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Meal\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!\n");
+    }
+}
